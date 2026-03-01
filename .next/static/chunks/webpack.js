443c77b664e2dc5a157bf918f4eabd7c72c54b68
geiner28 <geiner.martinez39145@ucaldas.edu.co>
2026-03-01 10:32:04 -0500

    camios 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -163,7 +163,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/chunks/" + chunkId + ".js";
+/******/ 			return undefined;
 /******/ 		};
 /******/ 	}();
 /******/ 	
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "50bb2b33c1e9b5a9"; }
+/******/ 		__webpack_require__.h = function() { return "a5320e3aa99f161c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
